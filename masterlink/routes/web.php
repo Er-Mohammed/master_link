ecd550aa86c1cffa4adbaa9f197098d4c5ca0d3c
Mohammed <alslwym053@gmail.com>
2026-08-03 16:57:04 +0300

    add files can ous sand to mailtrap

--- masterlink/routes/web.php
+++ masterlink/routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
 
 Route::inertia('/', 'welcome')->name('home');
-
 
 require __DIR__.'/admin.php';
 
 require __DIR__.'/settings.php';
+
+Route::get('/test-mail', function () {
+
+    Mail::to('test@example.com')->send(new TestMail());
+
+    return 'Email sent successfully!';
+
+});
